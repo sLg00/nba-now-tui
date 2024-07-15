c9c65b35765b86da8bea4563f1e89d437f24af34
omar_t <sulg00@gmail.com>
2024-07-16 00:06:39 +0300

    Added test cases for type conversions

--- app/datamodels/genericModels_test.go
+++ app/datamodels/genericModels_test.go
@@ -34,5 +34,36 @@ func TestUnmarshallRequestJSON(t *testing.T) {
 			}
 		})
 	}
+}
 
+func TestStructToStringSlice(t *testing.T) {
+
+	type args struct {
+		intArg    int
+		floatArg  float64
+		stringArg string
+	}
+
+	testArgs := args{
+		intArg:    1,
+		floatArg:  1.00,
+		stringArg: "one",
+	}
+
+	var tests = []struct {
+		name  string
+		input interface{}
+		want  []string
+	}{
+		{"coveredtypes", testArgs, []string{"1", "1.00", "one"}},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			result := structToStringSlice(test.input)
+			if !reflect.DeepEqual(result, test.want) {
+				t.Errorf("Wanted: %v, Got: %v", test.want, result)
+			}
+		})
+	}
 }
