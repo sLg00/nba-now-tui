f68d6919828d1d71ca71299fd8a19d193a365af1
omar_t <sulg00@gmail.com>
2024-07-19 17:17:53 +0300

    Refactored table rendering to support pagination and srolling

--- tui/leagueLeaders.go
+++ tui/leagueLeaders.go
@@ -3,13 +3,14 @@ package tui
 import (
 	"github.com/charmbracelet/bubbles/key"
 	"github.com/charmbracelet/bubbles/list"
-	"github.com/charmbracelet/bubbles/table"
-	tea "github.com/charmbracelet/bubbletea"
-	"github.com/charmbracelet/lipgloss"
-	"github.com/sLg00/nba-now-tui/app/datamodels"
-	"log"
 	"slices"
 	"strings"
+
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+	"github.com/evertras/bubble-table/table"
+	"github.com/sLg00/nba-now-tui/app/datamodels"
+	"log"
 )
 
 // removeIndex is a helper function to delete elements from a slice
@@ -20,6 +21,10 @@ func removeIndex[T any](slice []T, s int) []T {
 type leagueLeaders struct {
 	leaderboard table.Model
 	quitting    bool
+	height      int
+	width       int
+	maxHeight   int
+	maxWidth    int
 }
 
 func (m leagueLeaders) Init() tea.Cmd { return nil }
@@ -27,87 +32,75 @@ func (m leagueLeaders) Init() tea.Cmd { return nil }
 // initLeagueLeadersTable returns a table.Model which is populated with the current league leaders (per PPG)
 func initLeagueLeaders(i list.Item, p *tea.Program) (*leagueLeaders, error) {
 	playerStats, headers, err := datamodels.PopulatePlayerStats()
+	log.Println("MINGE MUNNI")
 	if err != nil {
 		log.Println("Could not populate player stats, error:", err)
 		return nil, err
 	}
 	playerStatsString := datamodels.ConvertToString(playerStats)
 
-	var (
-		columns    []table.Column
-		column     table.Column
-		rows       []table.Row
-		row        table.Row
-		sortedRows []table.Row
-	)
-
+	var filteredHeaders []string
 	for _, h := range headers {
-		column = table.Column{
-			Title: h,
-			Width: 10,
+		if !strings.Contains(h, "ID") {
+			filteredHeaders = append(filteredHeaders, h)
 		}
-		if strings.Contains(column.Title, "PLAYER") {
-			column.Width = 25
-		}
-		columns = append(columns, column)
 	}
 
-	// filter out "ID" slices
-	for _, r := range playerStatsString {
-		row = r
-		//row = removeIndex(row, 0)
-		//row = removeIndex(row, 2)
-		//delete ID slices
-		row = slices.Delete(row, 0, 1)
-		row = slices.Delete(row, 2, 3)
+	var filteredRows [][]string
+	for _, fr := range playerStatsString {
+		fr = slices.Delete(fr, 0, 1)
+		fr = slices.Delete(fr, 2, 3)
+		filteredRows = append(filteredRows, fr)
+	}
+
+	var sortedHeaders []string
+	sortedHeaders = append(sortedHeaders, filteredHeaders[0:5]...)
+	sortedHeaders = append(sortedHeaders, filteredHeaders[21:23]...)
+	sortedHeaders = append(sortedHeaders, filteredHeaders[17:21]...)
+	sortedHeaders = append(sortedHeaders, filteredHeaders[5:17]...)
+
+	var sortedRows [][]string
+	for _, r := range filteredRows {
+		var sorterRow []string
+		sorterRow = append(sorterRow, r[0:5]...)
+		sorterRow = append(sorterRow, r[21:23]...)
+		sorterRow = append(sorterRow, r[17:21]...)
+		sorterRow = append(sorterRow, r[5:17]...)
+		sortedRows = append(sortedRows, sorterRow)
+	}
+
+	var (
+		columns []table.Column
+		column  table.Column
+		rows    []table.Row
+		row     table.Row
+	)
+
+	for _, h := range sortedHeaders {
+		column = table.NewColumn(h, h, 15)
+		columns = append(columns, column)
+
+	}
+
+	for _, r := range sortedRows {
+		rowData := make(table.RowData)
+		for i, rd := range r {
+			columnTitle := columns[i].Title()
+			rowData[columnTitle] = rd
+		}
+		row = table.NewRow(rowData)
 		rows = append(rows, row)
 	}
 
-	//sort the data in a logical order with major categories at the front
-	for _, r := range rows {
-		row = r
-		var sortedRow table.Row
-		sortedRow = append(sortedRow, row[0:5]...)
-		sortedRow = append(sortedRow, row[21:23]...)
-		sortedRow = append(sortedRow, row[17:21]...)
-		sortedRow = append(sortedRow, row[5:17]...)
-		sortedRows = append(sortedRows, sortedRow)
-	}
+	t := table.New(columns).
+		WithRows(rows).
+		WithHeaderVisibility(true).
+		Focused(true).
+		WithPageSize(20).
+		WithMaxTotalWidth(150).
+		WithHorizontalFreezeColumnCount(2).WithBaseStyle(lipgloss.NewStyle().BorderStyle(lipgloss.DoubleBorder()))
 
-	// for loop to filter out ID column headers
-	var filteredColumns []table.Column
-	for _, c := range columns {
-		if !strings.Contains(c.Title, "ID") {
-			filteredColumns = append(filteredColumns, c)
-		}
-	}
-
-	//sort columns in a more logical order, with major categories at the beginning
-	var sortedColumns []table.Column
-	sortedColumns = append(sortedColumns, filteredColumns[0:5]...)
-	sortedColumns = append(sortedColumns, filteredColumns[21:23]...)
-	sortedColumns = append(sortedColumns, filteredColumns[17:21]...)
-	sortedColumns = append(sortedColumns, filteredColumns[5:17]...)
-
-	t := table.New(
-		table.WithColumns(sortedColumns),
-		table.WithRows(sortedRows),
-		table.WithFocused(true),
-		table.WithHeight(25),
-	)
-
-	s := table.DefaultStyles()
-	s.Header = s.Header.
-		BorderStyle(lipgloss.DoubleBorder()).
-		BorderForeground(lipgloss.Color("240")).
-		BorderBottom(true).
-		Bold(true)
-	s.Selected = s.Selected.
-		Foreground(lipgloss.Color("229")).
-		Background(lipgloss.Color("57")).
-		Bold(false)
-	t.SetStyles(s)
-	m := &leagueLeaders{leaderboard: t}
+	m := &leagueLeaders{leaderboard: t, maxHeight: 25, maxWidth: 80}
 
 	return m, nil
 }
@@ -123,7 +116,18 @@ func (m leagueLeaders) Update(msg tea.Msg) (model tea.Model, cmd tea.Cmd) {
 			m.quitting = true
 			return m, tea.Quit
 		}
+	case tea.WindowSizeMsg:
+		m.width = msg.Width
+		m.height = msg.Height
+
+		if m.width > m.maxWidth {
+			m.maxWidth = m.width
+		}
+		if m.height > m.maxHeight {
+			m.maxHeight = m.height
+		}
 	}
+
 	m.leaderboard, cmd = m.leaderboard.Update(msg)
 	cmds = append(cmds, cmd)
 	return m, tea.Batch(cmds...)
