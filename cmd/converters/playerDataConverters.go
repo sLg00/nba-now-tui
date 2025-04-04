bf01886ff30aa502e635b1ebfd2046b702b51d5f
omar_t <sulg00@gmail.com>
2025-04-05 00:55:46 +0300

    Refactored the converter logic of players to be less of a turd.

--- cmd/converters/playerDataConverters.go
+++ cmd/converters/playerDataConverters.go
@@ -1,6 +1,7 @@
 package converters
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/sLg00/nba-now-tui/cmd/nba/types"
 	"log"
@@ -8,79 +9,33 @@ import (
 
 // PopulatePlayerStats maps the data to the Player struct
 func PopulatePlayerStats(rs types.ResponseSet) (types.Players, []string, error) {
-	response := rs
-
 	// mapping as Player types
-	headers := response.ResultSet.Headers
+	headers := rs.ResultSet.Headers
 	var playerStats types.Players
-	for _, row := range response.ResultSet.RowSet {
+	for _, row := range rs.ResultSet.RowSet {
 		if len(row) != len(headers) {
-			err := fmt.Errorf("row length doesn't match headers length. %v != %v", len(row), len(headers))
+			log.Println("len(row) != len(headers):", len(row), len(headers))
+			return nil, nil, fmt.Errorf("row length doesn't match headers length. %v != %v", len(row), len(headers))
+		}
+
+		playerData := make(map[string]interface{})
+		for i, value := range row {
+			playerData[headers[i]] = value
+		}
+
+		jsonData, err := json.Marshal(playerData)
+		if err != nil {
 			log.Println(err)
-			return nil, nil, err
+			return nil, nil, fmt.Errorf("failed to marshal player data. %v", err)
 		}
 
 		var player types.Player
-		for i, value := range row {
-			switch v := value.(type) {
-			case float64:
-				switch headers[i] {
-				case "PLAYER_ID":
-					player.PlayerID = int(v)
-				case "RANK":
-					player.Rank = int(v)
-				case "GP":
-					player.GamesPlayed = int(v)
-				case "TEAM_ID":
-					player.TeamID = int(v)
-				case "MIN":
-					player.Minutes = v
-				case "FGM":
-					player.FGM = v
-				case "FGA":
-					player.FGA = v
-				case "FG_PCT":
-					player.FGPCT = v
-				case "FG3M":
-					player.FG3PTM = v
-				case "FG3A":
-					player.FG3PTA = v
-				case "FG3_PCT":
-					player.FG3PTPCT = v
-				case "FTM":
-					player.FTM = v
-				case "FTA":
-					player.FTA = v
-				case "FT_PCT":
-					player.FTPCT = v
-				case "OREB":
-					player.OREB = v
-				case "DREB":
-					player.DREB = v
-				case "REB":
-					player.REB = v
-				case "AST":
-					player.AST = v
-				case "STL":
-					player.STL = v
-				case "BLK":
-					player.BLK = v
-				case "TOV":
-					player.TOV = v
-				case "PTS":
-					player.PTS = v
-				case "EFF":
-					player.EFF = v
-				}
-			case string:
-				switch headers[i] {
-				case "PLAYER":
-					player.PlayerName = v
-				case "TEAM":
-					player.TeamAbbr = v
-				}
-			}
+		err = json.Unmarshal(jsonData, &player)
+		if err != nil {
+			log.Println(err)
+			return nil, nil, fmt.Errorf("failed to unmarshal player data. %v", err)
 		}
+
 		playerStats = append(playerStats, player)
 	}
 	return playerStats, headers, nil
