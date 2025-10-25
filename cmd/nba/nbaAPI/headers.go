e93489c03b43f3e98466169b6221a28e6baca976
omar_t <sulg00@gmail.com>
2025-10-25 16:51:41 +0300

    Updated headers to comply with new /stats endpoints requirements

--- cmd/nba/nbaAPI/headers.go
+++ cmd/nba/nbaAPI/headers.go
@@ -5,7 +5,7 @@ import "net/http"
 // SetHeaders returns the required headers to query NBA APIs
 func (h *HTTPClient) SetHeaders() http.Header {
 	return http.Header{
-		"User-Agent":         {"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:72.0), Gecko/20100101, Firefox/72.0"},
+		"User-Agent":         {"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36"},
 		"Accept":             {"application/json; charset=utf-8 , text/plain, */*"},
 		"Accept-Language":    {"en-US, en;q=0.5firefox-125.0b3.tar.bz2"},
 		"Accept-Encoding":    {"deflate, br"},
@@ -15,5 +15,8 @@ func (h *HTTPClient) SetHeaders() http.Header {
 		"Referer":            {"https://stats.nba.com/"},
 		"Pragma":             {"no-cache"},
 		"Cache-Control":      {"no-cache"},
+		"Sec-Ch-Ua":          {`"Chromium";v="140", "Google Chrome";v="140", "Not;A=Brand";v="24"`},
+		"Sec-Ch-Ua-Mobile":   {"?0"},
+		"Sec-Fetch-Dest":     {"empty"},
 	}
 }
